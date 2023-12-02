4df5b02e580ac374e5d83a1fbab7682432927d45
Havrylenko Ivan (WSL) <havrylenko.inbox@gmail.com>
2023-12-02 17:03:51 +0200

    test(reply use case): add cases for all methods

--- src/use-cases/reply/tests/reply.use-case.spec.ts
+++ src/use-cases/reply/tests/reply.use-case.spec.ts
@@ -1,15 +1,29 @@
 import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
-import { IReplyService, IReportService } from 'src/core/abstracts';
-import { Keyboards } from 'src/core/constants';
-import { Extra, MsgKey } from 'src/types';
+import { IReplyService } from 'src/core/abstracts';
+import { IReportService } from 'src/core/abstracts';
+import { Profile, Report, ReportsChannel, User } from 'src/core/entities';
+import { Language, MsgKey } from 'src/types';
 import { Context } from 'telegraf';
 
 import { ReplyUseCases } from '../reply.use-case';
 
+jest.mock('src/core/utils', () => {
+  const originalModule = jest.requireActual('telegraf');
+
+  // Mock the default export
+  return {
+    __esModule: true,
+    ...originalModule,
+    getReportCaption: jest.fn(),
+    getReportMarkup: jest.fn(),
+  };
+});
+
 describe('ReplyUseCases', () => {
   let useCases: ReplyUseCases;
   let replyService: IReplyService;
+  let reportService: IReportService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,19 +42,104 @@ describe('ReplyUseCases', () => {
 
     useCases = module.get<ReplyUseCases>(ReplyUseCases);
     replyService = module.get<IReplyService>(IReplyService);
+    reportService = module.get<IReportService>(IReportService);
   });
 
-  describe('replyI18n', () => {
-    it('should call reply on the reply service with the correct arguments', async () => {
-      const ctx = createMock<Context>();
-      const key: MsgKey = 'messages.age.send';
-      const params: Extra = {
-        reply_markup: Keyboards.remove,
-      };
+  it('should reply with i18n', async () => {
+    const ctx = createMock<Context>();
+    const key: MsgKey = 'messages.about';
+    const params = {};
 
-      await useCases.replyI18n(ctx, key, params);
+    await useCases.replyI18n(ctx, key, params);
 
-      expect(replyService.reply).toHaveBeenCalledWith(ctx, key, params);
+    expect(replyService.reply).toHaveBeenCalledWith(ctx, key, params);
+  });
+
+  it('should send a message to chat', async () => {
+    const chatId = 1;
+    const msg = 'test';
+    const args = {};
+
+    await useCases.sendMsgToChat(chatId, msg, args);
+
+    expect(replyService.sendPhotoToChat).toHaveBeenCalledWith(
+      chatId,
+      msg,
+      args,
+    );
+  });
+
+  it('should send a message to chat with i18n', async () => {
+    const chatId = 1;
+    const key: MsgKey = 'messages.about';
+    const args = {};
+
+    await useCases.sendMsgToChatI18n(chatId, key, args);
+
+    expect(replyService.sendMsgToChatI18n).toHaveBeenCalledWith(
+      chatId,
+      key,
+      args,
+    );
+  });
+
+  it('should send a photo to chat', async () => {
+    const chatId = 1;
+    const fileId = 'test';
+    const args = {};
+
+    await useCases.sendPhotoToChat(chatId, fileId, args);
+
+    expect(replyService.sendPhotoToChat).toHaveBeenCalledWith(
+      chatId,
+      fileId,
+      args,
+    );
+  });
+
+  it('should send to reports channel', async () => {
+    const report: Report = createMock<Report>({
+      description: 'Test',
+      reporter: createMock<User>({
+        id: 1,
+      }),
+      user: createMock<User>({
+        id: 2,
+        profile: createMock<Profile>({
+          fileId: '12345',
+        }),
+      }),
     });
+    const reportsChannel = createMock<ReportsChannel>({
+      id: 1,
+    });
+
+    const findReportsChannelSpy = jest
+      .spyOn(reportService, 'findReportsChannel')
+      .mockResolvedValue(reportsChannel);
+
+    await useCases.sendToReportsChannel(report);
+
+    expect(findReportsChannelSpy).toHaveBeenCalled();
+    expect(replyService.sendPhotoToChat).toHaveBeenCalledWith(
+      reportsChannel.id,
+      report.user.profile.fileId,
+      expect.anything(),
+    );
+  });
+
+  it('should translate', () => {
+    const key: MsgKey = 'messages.about';
+    const lang = Language.EN;
+    const i18nArgs = {};
+
+    const translateSpy = jest
+      .spyOn(replyService, 'translate')
+      .mockReturnValue('translated');
+
+    const result = useCases.translate(key, lang, i18nArgs);
+
+    expect(result).toEqual('translated');
+    expect(translateSpy).toHaveBeenCalledWith(key, lang, i18nArgs);
   });
 });
